d5cf328cb9fcab331a973370737b13dd499fe199
Juan Real <juan.real@canonical.com>
2018-09-10 14:50:28 +0100

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'onut',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+  {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      trackingId: 'UA-125510208-1',
+      head: false
+    },
+  }
+  ],
 }

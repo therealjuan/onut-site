f75b31b92bdd03a66766f4cc40da761f593525ed
Juan Real <juan.real@canonical.com>
2019-11-09 19:04:36 +0000

    Updated video

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import videoSource from './water.mp4'
+import videoPoster from './water.gif'
 import './Video.css'
 
 const Video = () => (
     <div className="video">
-        <video autoPlay muted loop id="videoBackground">
+        <video autoPlay muted loop id="videoBackground" poster={videoPoster}>
             <source src={videoSource} type="video/mp4"></source>
         </video>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Video from '../components/video'
+import Video from '../components/video/video'
 import Header from '../components/header'
 
 import './Home.css'

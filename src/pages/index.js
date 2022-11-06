22db2b15027a4977fb69ecdabd18e4e9ad715a01
Juan Real <juanreal@Juans-MacBook-Pro.local>
2022-11-06 17:36:26 +0100

    Updated homepage with our details

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,10 @@ export default props => {
   <Layout location={props.location}>
     <Video />
     <Header />
+    <div className="details">
+        <h4>50% <a href="https://alixmzmz.eu" target="_blank">Alix Martínez Martínez</a></h4>
+        <h4>50% <a href="https://juanreal.com" target="_blank">Juan Real Garry</a></h4>
+    </div>
   </Layout>
   )
 }
